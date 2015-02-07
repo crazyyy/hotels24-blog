3e1f5c9f6d16be3e02dd648cb9c7075c4cec102b
Vitaly A <crazyyy@gmail.com>
2015-02-07 14:23:04 +0200

    bubenchik

--- wp-hotels/index.php
+++ wp-hotels/index.php
@@ -1,17 +1,173 @@
-<?php get_header(); ?>
-	
-	<!-- section -->
-	<section role="main">
-	
-		<h1 class="ctitle"><?php _e( 'Latest Posts', 'wpeasy' ); ?></h1>
-	
-		<?php get_template_part('loop'); ?>
-		
-		<?php get_template_part('pagination'); ?>
-	
-	</section>
-	<!-- /section -->
-	
-<?php get_sidebar(); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<!-- dns prefetch -->
+		<link href="http://www.google-analytics.com/" rel="dns-prefetch">
+		<!-- meta -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<!-- icons -->
+		<link href="favicon.ico" rel="shortcut icon">
+		<!-- css + javascript -->
+		<link rel="stylesheet" href="style.css" media="all">
+		<script type="text/javascript" src="js/modernizr.min.js"></script>
 
-<?php get_footer(); ?>
+	</head>
+	<body>
+		<!-- wrapper -->
+		<div class="wrapper clearfix">
+			<!-- header -->
+			<header class="header clearfix">
+				<div class="top-header">
+					<!-- nav -->
+					<nav class="nav" role="navigation">
+						<ul>
+							<li><a href="">Все новости</a></li>
+							<li><a href="">Отдых в Украине</a></li>
+							<li><a href="">Отдых за рубежом</a></li>
+							<li><a href="">Командировка</a></li>
+							<li><a class="nav-more" href="">Еще...</a></li>
+						</ul>
+					</nav>
+					<!-- /nav -->
+					<form action="">
+						<input type="text"><button></button>
+					</form>
+					<ul class="lang-selector">
+						<li>Ukr</li>
+						<li>Rus</li>
+					</ul>
+				</div>
+				<!-- /.top-header -->
+				<div class="nav-more-block">
+					<span>Еще...</span>
+					<ul>
+						<li><a href="">Интересные факты</a></li>
+						<li><a href="">Новости партнеров</a></li>
+						<li><a href="">Подборки отелей</a></li>
+						<li><a href="">Закулисье Hotels24.ua</a></li>
+						<li><a href="">Пример второго ряда</a></li>
+					</ul>
+				</div>
+				<!-- /.nav-more-block -->
+				<!-- logo -->
+				<div class="logo">
+					<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+					<img src="img/logo.png" alt="-" title="-" class="logo-img">
+				</div>
+				<!-- /logo -->
+				<div class="advanced">
+					
+				</div>
+				<!-- /.advanced -->
+			</header>
+			<!-- /header -->
+
+			<div class="container">
+
+				<!-- section -->
+				<section role="main">
+					
+					<h1 class="ctitle">Последние записи</h1>
+					
+					
+					<!-- article -->
+					<article>
+						<h2 class="title">Извините, нечего показывать ;(</h2>
+					</article>
+					<!-- /article -->
+					
+					<!-- pagination -->
+					<div class="pagination">
+						<!-- noindex --><!-- /noindex -->
+					</div>
+					<!-- /pagination -->
+				</section>
+				<!-- /section -->
+				
+				<!-- sidebar -->
+				<aside class="sidebar" role="complementary">
+					<div class="widget widget-anonses">
+						<h6>Афиша событий <a href="">смотреть все</a></h6>
+						<ul>
+							<li class="today">
+								<h6>Сегодня</h6>
+								<a href="">Новый год во Львове: подарите близким средневековую сказку! </a>
+							</li>
+						</ul>
+					</div><!-- widget widget-anonses -->
+					<div class="widget widget-popular">
+						<h5>Популярное</h5>
+						<ul>
+							<li>
+								<img src="" alt="">
+								<a href="">Новый год во Львове: подарите близким средневековую сказку!</a>
+							</li>
+							<li>
+								<img src="" alt="">
+								<a href="">Вперед в прошлое: уникальный музей ретро-автомобилей в Запорожье </a>
+							</li>
+							<li>
+								<img src="" alt="">
+								<a href="">Новый 2015 год в Буковеле. Актуальные цены на проживание</a>
+							</li>
+							<li>
+								<img src="" alt="">
+								<a href="">10 способов сэкономить на путешествии по Европе</a>
+							</li>
+							<li>
+								<img src="" alt="">
+								<a href="">Достопримечательности Центральной Украины: путешествие по интересным местам 5 областей</a>
+							</li>
+							<li>
+								<img src="" alt="">
+								<a href="">Новый год во Львове: подарите близким средневековую сказку! </a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.widget widget-popular -->
+				</aside>
+				<!-- /sidebar -->
+				
+			</div>
+			<!-- /.container -->
+			
+		</div>
+		<!-- /wrapper -->
+
+		<!-- footer -->
+		<footer class="footer" role="contentinfo">
+			
+			<div class="mainfooter">
+				<!-- copyright -->
+				<p class="copyright">
+					Перепечатка и использование любых материалов, размещенных на нашем сайте, возможны только с письменного разрешения, с указанием названия источника и наличием активной гиперссылки на Hotels24.ua.
+				</p>
+				<p>По вопросам сотрудничества пишите:  viktoriya.dovhan@hotels24.ua</p>
+				<!-- /copyright -->
+			</div>
+			<!-- /.mainfooter -->
+			
+		</footer>
+		<!-- /footer -->
+
+		<div class="backhead bkblock0n">
+		</div><!-- backhead bkblock0n -->
+		<div class="backhead bkblock01">
+			<div class="shadow"></div>
+		</div><!-- backhead bkblock01 -->
+
+		<script type="text/javascript" src="js/jquery.min.js"></script>		
+		<script type="text/javascript" src="js/app.js"></script>
+
+		<!-- analytics -->
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)})(document,'script');
+		</script>
+	</body>
+</html>
