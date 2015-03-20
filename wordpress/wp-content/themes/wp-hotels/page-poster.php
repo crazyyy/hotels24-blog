fd4430eb82901f1d6aeef199846d4a0e610d5201
crazyyy <crazyyy@gmail.com>
2015-03-20 14:42:40 +0200

    before repair query

--- wordpress/wp-content/themes/wp-hotels/page-poster.php
+++ wordpress/wp-content/themes/wp-hotels/page-poster.php
@@ -21,10 +21,7 @@ $args = array (
     ),
 );
 ?>
-	<?php
-		query_posts($args); 
-	?>
-
+	<?php query_posts($args); ?>
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	ssss9111
